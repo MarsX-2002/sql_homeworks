47e58cd6c1d9dbe0964474b7c1b46f9e38e36968
MarsX-2002 <cobocoinweb3@gmail.com>
2025-03-20 17:21:11 +0500

    lesson12 bug fix

--- lesson-12/homework/lesson12_t1.sql
+++ lesson-12/homework/lesson12_t1.sql
@@ -1,16 +1,19 @@
-EXEC sp_MSforeachdb '
-IF ''?'' NOT IN (''master'', ''tempdb'', ''model'', ''msdb'')
-BEGIN
-    USE [?];
+DECLARE @SQL NVARCHAR(MAX) = N'';
+
+SELECT @SQL = @SQL + '
     SELECT 
-        ''?'' AS DatabaseName,
+        ''' + name + ''' AS DatabaseName,
         s.name AS SchemaName,
         t.name AS TableName,
         c.name AS ColumnName,
         ty.name AS DataType
-    FROM sys.tables t
-    INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
-    INNER JOIN sys.columns c ON t.object_id = c.object_id
-    INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+    FROM [' + name + '].sys.tables t
+    INNER JOIN [' + name + '].sys.schemas s ON t.schema_id = s.schema_id
+    INNER JOIN [' + name + '].sys.columns c ON t.object_id = c.object_id
+    INNER JOIN [' + name + '].sys.types ty ON c.user_type_id = ty.user_type_id
     ORDER BY s.name, t.name, c.column_id;
-END';
+'
+FROM sys.databases
+WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');
+
+EXEC sp_executesql @SQL;
--- lesson-12/homework/lesson12_t2.sql
+++ lesson-12/homework/lesson12_t2.sql
@@ -1,52 +1,55 @@
+IF OBJECT_ID('dbo.GetRoutinesAndParameters', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.GetRoutinesAndParameters;
+GO
+
 CREATE PROCEDURE dbo.GetRoutinesAndParameters
     @DatabaseName SYSNAME = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    DECLARE @SQL NVARCHAR(MAX);
+
     IF @DatabaseName IS NOT NULL
     BEGIN
-        DECLARE @sql NVARCHAR(MAX);
-
-        SET @sql = N'
-        USE [' + QUOTENAME(@DatabaseName) + N'];
-        SELECT 
-            ''' + @DatabaseName + N''' AS DatabaseName,
-            r.ROUTINE_SCHEMA AS SchemaName,
-            r.ROUTINE_NAME AS RoutineName,
-            r.ROUTINE_TYPE AS RoutineType,
-            p.PARAMETER_NAME AS ParameterName,
-            p.DATA_TYPE AS ParameterDataType,
-            p.CHARACTER_MAXIMUM_LENGTH AS ParameterMaxLength
-        FROM INFORMATION_SCHEMA.ROUTINES r
-        LEFT JOIN INFORMATION_SCHEMA.PARAMETERS p
-            ON r.SPECIFIC_NAME = p.SPECIFIC_NAME
-        ORDER BY r.ROUTINE_SCHEMA, r.ROUTINE_NAME, p.ORDINAL_POSITION;';
-        
-        EXEC sp_executesql @sql;
+        SET @SQL = N'
+            SELECT 
+                ''' + @DatabaseName + ''' AS DatabaseName,
+                r.ROUTINE_SCHEMA,
+                r.ROUTINE_NAME,
+                r.ROUTINE_TYPE,
+                p.PARAMETER_NAME,
+                p.DATA_TYPE,
+                p.CHARACTER_MAXIMUM_LENGTH
+            FROM [' + @DatabaseName + '].INFORMATION_SCHEMA.ROUTINES r
+            LEFT JOIN [' + @DatabaseName + '].INFORMATION_SCHEMA.PARAMETERS p
+                ON r.SPECIFIC_NAME = p.SPECIFIC_NAME
+            ORDER BY r.ROUTINE_SCHEMA, r.ROUTINE_NAME, p.ORDINAL_POSITION;
+        ';
+        EXEC sp_executesql @SQL;
     END
     ELSE
     BEGIN
-        -- Using sp_MSforeachdb to loop through all non-system databases
-        DECLARE @cmd NVARCHAR(MAX) = N' 
-        IF DB_ID(''?'' ) NOT IN (DB_ID(''master''), DB_ID(''tempdb''), DB_ID(''model''), DB_ID(''msdb''))
-        BEGIN
-            USE [?];
+        SET @SQL = N'';
+        
+        SELECT @SQL = @SQL + '
             SELECT 
-                DB_NAME() AS DatabaseName,
-                r.ROUTINE_SCHEMA AS SchemaName,
-                r.ROUTINE_NAME AS RoutineName,
-                r.ROUTINE_TYPE AS RoutineType,
-                p.PARAMETER_NAME AS ParameterName,
-                p.DATA_TYPE AS ParameterDataType,
-                p.CHARACTER_MAXIMUM_LENGTH AS ParameterMaxLength
-            FROM INFORMATION_SCHEMA.ROUTINES r
-            LEFT JOIN INFORMATION_SCHEMA.PARAMETERS p
+                ''' + name + ''' AS DatabaseName,
+                r.ROUTINE_SCHEMA,
+                r.ROUTINE_NAME,
+                r.ROUTINE_TYPE,
+                p.PARAMETER_NAME,
+                p.DATA_TYPE,
+                p.CHARACTER_MAXIMUM_LENGTH
+            FROM [' + name + '].INFORMATION_SCHEMA.ROUTINES r
+            LEFT JOIN [' + name + '].INFORMATION_SCHEMA.PARAMETERS p
                 ON r.SPECIFIC_NAME = p.SPECIFIC_NAME
             ORDER BY r.ROUTINE_SCHEMA, r.ROUTINE_NAME, p.ORDINAL_POSITION;
-        END';
-        
-        EXEC sp_MSforeachdb @cmd;
+        '
+        FROM sys.databases
+        WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');
+
+        EXEC sp_executesql @SQL;
     END
 END
 GO
